32fed7fb6f18ae2147f75518948bef3e564c5807
Alex Bechmann <alex_bechmann@hotmail.com>
2015-05-11 18:06:39 +0200

    compression works but decrypt now doesn't - commit before reverting to swift files (no pods)

---
+++ CompresJSON/CompresJSON/Library/Compressor.swift
@@ -0,0 +1,13 @@
+//
+//  Compressor.swift
+//  CompresJSON
+//
+//  Created by Alex Bechmann on 11/05/2015.
+//  Copyright (c) 2015 Alex Bechmann. All rights reserved.
+//
+
+import UIKit
+
+class Compressor: NSObject {
+   
+}
